da23421e8293b533a62a2cf6267b39fbe323bf48
Esteban Flores <esteban@teknol.com.mx>
2017-01-23 17:30:27 -0700

    Cambios impresion

--- server/main.js
+++ server/main.js
@@ -59,10 +59,10 @@ io.on('connection', function(socket)
 		});
 
 		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Pago',45);	
+			.text('2 ó más trámites',45);	
 		
 		myDoc.end();
 	});
@@ -97,7 +97,7 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
 			.text('Contrato',45);
@@ -135,7 +135,7 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
 			.text('Convenio',45);
@@ -173,10 +173,10 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Cambio de Nombre',45);
+			.text('Cambio de nombre',45);
 
 		myDoc.end();
 	});
@@ -211,10 +211,10 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Carta de No Adeudo',45);
+			.text('Carta de no adeudo',45);
 
 		myDoc.end();
 	});
@@ -249,10 +249,10 @@ io.on('connection', function(socket)
 		});
 
 		myDoc.font('Times-Roman',12)
-			.text('Pagos Y Tramites',45);
+			.text('Trámites',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Factibilidad de Servicios',45);	
+			.text('Factibilidad de servicios',45);	
 		
 		myDoc.end();
 	});
@@ -289,10 +289,10 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
+			.text('Aclaraciones y otros',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Alto Consumo (con medidor)',45);
+			.text('Alto consumo (con y sin medidor)',45);
 		
 		myDoc.end();
 	});
@@ -327,86 +327,10 @@ io.on('connection', function(socket)
 		});
 
 		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
+			.text('Aclaraciones y otros',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Reconexion de Servicio',45);
-		
-		myDoc.end();
-	});
-	socket.on('alto_consumo_estimado',function(imprimir){
-		console.log('Creando PDF: '+ imprimir);
-		
-		var myDoc = new pdf;
-		myDoc.pipe(fs.createWriteStream('node.pdf'));
-		
-		myDoc.font("Times-Roman",12)
-			.image('img/logo_aguaa.png',75,10,{
-				 width: 60, 
-				 height: 60
-			})
-
-			.moveDown(0.5)
-			.text("Agua de Hermosillo a:",50)
-			.moveDown(0.5);
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
-			.moveDown(0.5);	
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
-			.moveDown(0.5);	
-				
-		myDoc.font('Times-Roman',12)
-			.fontSize(48)
- 			.text(imprimir,{
-				align: 'left'
-		});
-
- 		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
-
-		myDoc.font('Times-Roman',8)
-			.text('Alto Consumo (estimado)',45);
-
-		myDoc.end();
-	});
-	socket.on('cambio_tarifa',function(imprimir){
-		console.log('Creando PDF: '+ imprimir);
-		
-		var myDoc = new pdf;
-		myDoc.pipe(fs.createWriteStream('node.pdf'));
-		
-		myDoc.font("Times-Roman",12)
-			.image('img/logo_aguaa.png',75,10,{
-				 width: 60, 
-				 height: 60
-			})
-
-			.moveDown(0.5)
-			.text("Agua de Hermosillo a:",50)
-			.moveDown(0.5);
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
-			.moveDown(0.5);	
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
-			.moveDown(0.5);	
-				
-		myDoc.font('Times-Roman',12)
-			.fontSize(48)
- 			.text(imprimir,{
-				align: 'left'
-		});
-
- 		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
-
-		myDoc.font('Times-Roman',8)
-			.text('Cambio de Tarifa',45);
+			.text('Reconexión de servicio',45);
 		
 		myDoc.end();
 	});
@@ -441,48 +365,10 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
+			.text('Aclaraciones y otros',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Error en Lectura',45);
-		
-		myDoc.end();
-	});
-	socket.on('correccion',function(imprimir){
-		console.log('Creando PDF: '+ imprimir);
-		
-		var myDoc = new pdf;
-		myDoc.pipe(fs.createWriteStream('node.pdf'));
-		
-		myDoc.font("Times-Roman",12)
-			.image('img/logo_aguaa.png',75,10,{
-				 width: 60, 
-				 height: 60
-			})
-
-			.moveDown(0.5)
-			.text("Agua de Hermosillo a:",50)
-			.moveDown(0.5);
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
-			.moveDown(0.5);	
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
-			.moveDown(0.5);	
-				
-		myDoc.font('Times-Roman',12)
-			.fontSize(48)
- 			.text(imprimir,{
-				align: 'left'
-		});
-
- 		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
-
-		myDoc.font('Times-Roman',8)
-			.text('Correccion de Datos',45);
+			.text('Error en lectura',45);
 		
 		myDoc.end();
 	});
@@ -517,13 +403,128 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
+			.text('Aclaraciones y otros',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('No Toma de Lectura',45);
+			.text('No toma de lectura',45);
 		
 		myDoc.end();
 	});
+	socket.on('no_entrega',function(imprimir){
+		console.log('Creando PDF: '+ imprimir);
+		
+		var myDoc = new pdf;
+		myDoc.pipe(fs.createWriteStream('node.pdf'));
+		
+		myDoc.font("Times-Roman",12)
+			.image('img/logo_aguaa.png',75,10,{
+				 width: 60, 
+				 height: 60
+			})
+
+			.moveDown(0.5)
+			.text("Agua de Hermosillo a:",50)
+			.moveDown(0.5);
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
+			.moveDown(0.5);	
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
+			.moveDown(0.5);	
+				
+		myDoc.font('Times-Roman',12)
+			.fontSize(48)
+ 			.text(imprimir,{
+				align: 'left'
+		});
+
+ 		myDoc.font('Times-Roman',12)
+			.text('Aclaraciones y otros',45);
+
+		myDoc.font('Times-Roman',8)
+			.text('No entrega de recibo',45);
+		
+		myDoc.end();
+	});
+	socket.on('cambio_tarifa',function(imprimir){
+		console.log('Creando PDF: '+ imprimir);
+		
+		var myDoc = new pdf;
+		myDoc.pipe(fs.createWriteStream('node.pdf'));
+		
+		myDoc.font("Times-Roman",12)
+			.image('img/logo_aguaa.png',75,10,{
+				 width: 60, 
+				 height: 60
+			})
+
+			.moveDown(0.5)
+			.text("Agua de Hermosillo a:",50)
+			.moveDown(0.5);
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
+			.moveDown(0.5);	
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
+			.moveDown(0.5);	
+				
+		myDoc.font('Times-Roman',12)
+			.fontSize(48)
+ 			.text(imprimir,{
+				align: 'left'
+		});
+
+ 		myDoc.font('Times-Roman',12)
+			.text('Aclaraciones y otros',45);
+
+		myDoc.font('Times-Roman',8)
+			.text('Cambio de tarifa',45);
+		
+		myDoc.end();
+	});
+	socket.on('solicitud',function(imprimir){
+		console.log('Creando PDF: '+ imprimir);
+		
+		var myDoc = new pdf;
+		myDoc.pipe(fs.createWriteStream('node.pdf'));
+		
+		myDoc.font("Times-Roman",12)
+			.image('img/logo_aguaa.png',75,10,{
+				 width: 60, 
+				 height: 60
+			})
+
+			.moveDown(0.5)
+			.text("Agua de Hermosillo a:",50)
+			.moveDown(0.5);
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
+			.moveDown(0.5);	
+
+		myDoc.font("Times-Roman",12)
+			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
+			.moveDown(0.5);	
+				
+		myDoc.font('Times-Roman',12)
+			.fontSize(48)
+ 			.text(imprimir,{
+				align: 'left'
+		});
+
+ 		myDoc.font('Times-Roman',12)
+			.text('Aclaraciones y otros',45);
+
+		myDoc.font('Times-Roman',8)
+			.text('Solicitud de medidor',45);
+		
+		myDoc.end();
+	});
+
 	socket.on('otros',function(imprimir){
 		console.log('Creando PDF: '+ imprimir);
 		
@@ -554,10 +555,10 @@ io.on('connection', function(socket)
 				align: 'left'
 		});
 		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones Y Otros',45);
+			.text('Aclaraciones y otros',45);
 
 		myDoc.font('Times-Roman',8)
-			.text('Otros Tramites',45);
+			.text('Otros trámites',45);
 
 		myDoc.end();
 	});
