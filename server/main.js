cd4a726701d470f810dbec3762e328356705e7f2
Esteban Flores <esteban@teknol.com.mx>
2017-09-21 10:27:10 -0700

    no message

--- server/main.js
+++ server/main.js
@@ -39,7 +39,7 @@ io.on('connection', function(socket)
 	/*Pagos*/
 	socket.on('pago',function(imprimir){
 
-		logger.info('Creando PDF: '+ imprimir +' de pagos');
+		logger.info('Creando PDF: '+ imprimir +' de pago o carta de no adeudo');
 		
 		var myDoc = new pdf;
 		myDoc.pipe(fs.createWriteStream('node.pdf'));
@@ -69,59 +69,17 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Pago',45);
+			.text('Pago o carta de no adeudo',45);
 
 		myDoc.end();
 
-		logger.info('Terminando de crear PDF: '+ imprimir +' de pagos');
+		logger.info('Terminando de crear PDF: '+ imprimir +' de pagos o carta de no adeudo');
 
 	});
-	/*tramites*/
-	socket.on('pdf',function(imprimir){
-
-		logger.info('Creando PDF: '+ imprimir +' de tramites');
-		
-		var myDoc = new pdf;
-		myDoc.pipe(fs.createWriteStream('node.pdf'));
-		
-		myDoc.font("Times-Roman",12)
-			.image('img/logo_aguaa.png',75,10,{
-				 width: 60, 
-				 height: 60
-			})
-
-			.moveDown(0.5)
-			.text("Agua de Hermosillo a:",50)
-			.moveDown(0.5);
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'D-MMMM-YY'),75)
-			.moveDown(0.5);	
-
-		myDoc.font("Times-Roman",12)
-			.text(fecha.format(new Date(), 'hh:mm:ss A'),70)
-			.moveDown(0.5);	
-				
-		myDoc.font('Times-Roman',12)
-			.fontSize(48)
- 			.text(imprimir,{
-				align: 'left'
-		});
-
-		myDoc.font('Times-Roman',12)
-			.text('Trámites',45);
-
-		myDoc.end();
-
-		logger.info('Terminando de crear PDF: '+ imprimir +' de tramites');
-
-	});
-	
-	/*Fin de pagos y tramites*/
 	/*aclaraciones y otros*/
-	socket.on('alto_consumo',function(imprimir){
+	socket.on('aclaracion',function(imprimir){
 
-		logger.info('Creando PDF: '+ imprimir +' de aclaraciones');
+		logger.info('Creando PDF: '+ imprimir +' de tramites o aclaraciones');
 		
 		var myDoc = new pdf;
 		myDoc.pipe(fs.createWriteStream('node.pdf'));
@@ -151,22 +109,22 @@ io.on('connection', function(socket)
 		});
 
  		myDoc.font('Times-Roman',12)
-			.text('Aclaraciones y otros',45);
+			.text('Trámites o Aclaraciones',45);
 
 		myDoc.end();
 
-		logger.info('Terminando de crear PDF: '+ imprimir +' de aclaraciones');
+		logger.info('Terminando de crear PDF: '+ imprimir +' de Trámites aclaraciones');
 	});
 	/*fin de aclaraciones y otros*/
 	socket.on('imprimir', function($numero){
 		exec('acrowrap.exe /acceptlicense /t "C:\\imprimir\\node.pdf" "BIXOLON"', (error, stdout, stderr) => {
-			logger.info('Imprimiendo numero: '+$numero.letra+''+$numero.numero+' de aclaraciones');
+			logger.info('Imprimiendo numero: '+$numero.letra+''+$numero.numero+'');
 			if (error) {
-    			console.error(`exec error: ${error}`);
+    			logger.info(`exec error: ${error}`);
     			return;
   			}
-  				console.log(`stdout: ${stdout}`);
-  				console.log(`stderr: ${stderr}`);
+  				logger.info(`stdout: ${stdout}`);
+  				logger.info(`stderr: ${stderr}`);
 		});
 	});
 });
